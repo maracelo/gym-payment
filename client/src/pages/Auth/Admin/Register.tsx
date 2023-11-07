0de76068f43f27c49eeb9cf38cb62ce2b5d3fef8
Marcelo Augusto <maraceloaugust@gmail.com>
2023-11-07 12:55:28 -0300

    Admin Auth pages added

--- client/src/pages/Auth/Admin/Register.tsx
+++ client/src/pages/Auth/Admin/Register.tsx
@@ -1,8 +1,26 @@
-
+import { Title, Container, Form, PassOption } from '../styled';
+import PasswordInput from '../../../components/PasswordInput';
+import { Link } from 'react-router-dom';
+import useAppSelector from '../../../redux/typedUseSelectorHook';
 
 function AdminRegister(){
+    const { dark } = useAppSelector((state) => state.darkMode);
+
     return (
-        <h1>Admin Register</h1>
+        <Container>
+            <Form>
+                <Title>Admin Register</Title>
+
+                <label> <input type="text" name='name' placeholder='Name' /> </label>
+                <label> <input type="email" name='email' placeholder='Email' /> </label>
+                <PasswordInput name='password' placeholder='Password' />
+                <PasswordInput name='confirm_password' placeholder='Confirm Password' />
+
+                <PassOption dark={dark}>
+                    Already have an account? <Link to={import.meta.env.VITE_BASE_URL + 'user/login'}>Click here.</Link>
+                </PassOption>
+            </Form>
+        </Container>
     );
 }
 
