d75e8dd2ced26508e5c22cd145c79d55d6bedfc0
Marcelo Augusto <maraceloaugust@gmail.com>
2023-11-07 12:45:38 -0300

    media queries added in Auth/styled.tsx

--- client/src/pages/Auth/styled.tsx
+++ client/src/pages/Auth/styled.tsx
@@ -6,11 +6,10 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
     height: calc(100% - 160px);
-    background-image: url(${import.meta.env.VITE_BASE_URL + 'public/assest/images/background.jpg'});
 `;
 
 export const Title = styled.h1`
-    font-size: 5vw;
+    font-size: 5vh;
     color: #fff;
     padding: 0;
     margin: 0;
@@ -41,6 +40,46 @@ export const Form = styled.form`
         font-size: 20px;
         margin: 20px 0;
         padding: 10px;
+    }
+
+    @media(max-height: 700px){
+        padding: 12px;
+
+        input{
+            font-size: 11px;
+            margin: 10px 0;
+            padding: 5px;
+            width: 100%;
+        }
+
+        .eye{
+            width: 12px;
+            height: 12px;
+            margin: 0;
+        }
+
+        p{
+            font-size: 12px;
+        }
+
+        label{
+
+            .eye{
+                margin-left: -20px;
+                float: right;
+            }
+        }
+    }
+
+    @media(max-width: 500px){
+        p{
+            font-size: 4vw;
+        }
+
+        input{
+            font-size: 11px;
+            margin: 12px 0;
+        }
     }
 `;
 
