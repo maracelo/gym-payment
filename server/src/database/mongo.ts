1eacf6f8dc9b2480272f06951ab22e43d944d911
Marcelo Augusto <maraceloaugust@gmail.com>
2023-11-13 00:41:19 -0300

    mongodb prod/dev/test options

--- server/src/database/mongo.ts
+++ server/src/database/mongo.ts
@@ -6,7 +6,22 @@ dotenv.config();
 async function MongoConnect(){
     try{
         console.log('Connecting to Mongo...');
-        await connect(process.env.MONGO_URL as string);
+        
+        let mongoUrl = '';
+
+        switch(process.env.NODE_ENV as string){
+            case 'production':
+                mongoUrl = process.env.MONGO_URL as string;
+            break;
+            case 'development':
+                mongoUrl = process.env.MONGO_DEV_URL as string;
+            break;
+            case 'test':
+                mongoUrl = process.env.MONGO_TEST_URL as string;
+        }
+
+        await connect(mongoUrl);
+
         console.log('Connected to Mongo');
     }catch(err){
         console.log(`Failed to connect: ${err}`)
