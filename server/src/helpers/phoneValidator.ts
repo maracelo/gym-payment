5753b469d8fb4bb161ee9e9254d84c0d059c8363
Marcelo Augusto <maraceloaugust@gmail.com>
2023-11-13 00:18:14 -0300

    type in phoneValidator

--- server/src/helpers/phoneValidator.ts
+++ server/src/helpers/phoneValidator.ts
@@ -1,7 +1,7 @@
 import { phone } from "phone";
 import validator from "validator";
 
-function phoneValidator(phoneN: string){
+function phoneValidator(phoneN: string): string | null{
     const filteredPhone = validator.whitelist(phoneN, '+0123456789')
 
     if(filteredPhone && phone(filteredPhone, {country: 'BR'}).isValid) return filteredPhone;
