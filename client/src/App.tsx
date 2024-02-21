f5fe59b7dec99079bf72397793a6fa939dcabef9
Marcelo Augusto <maracelojob@gmail.com>
2024-02-21 00:53:53 -0300

    $ added to unknown dark

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const darkState = useAppSelector((state) => state.darkMode);
 
   return (
-    <MainContainer dark={darkState.dark ? 'true' : 'false'}>
+    <MainContainer $dark={darkState.dark ? 'true' : 'false'}>
       <Header />
       <div className="container">
         <RouteList />
