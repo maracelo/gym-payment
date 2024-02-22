7d22ebe662ad0041c11944a2d1d5158540dc4324
Marcelo Augusto <maracelojob@gmail.com>
2024-02-21 21:20:15 -0300

    User profile page done

--- client/src/pages/Profile/User/index.tsx
+++ client/src/pages/Profile/User/index.tsx
@@ -1,33 +1,168 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
 import { Container, Info } from "../styled";
-import { useParams } from "react-router-dom";
+
+import useAppSelector from "../../../redux/typedUseSelectorHook";
+import { setAccessToken } from "../../../redux/reducers/accessTokenReducer";
+
+import checkAccessToken from "../../../helpers/checkAccessToken";
+import getAccessToken from "../../../helpers/getAccessToken";
+
+type User = {
+  name: string,
+  email: string,
+  phone: string|null,
+  plan: 'normal'|'vip'|'',
+  profile_pic: string
+};
 
 function User(){
   const { id } = useParams();
+  
   const [showDelForm, setShowDelForm] = useState(false);
   const [showEditForm, setShowEditForm] = useState(false);
   const [showCamIcon, setShowCamIcon] = useState(false);
+  
+  const accessTState = useAppSelector(state => state.accessToken);
+  const [userInfo, setUserInfo] = useState<any>({name: '', email: '', phone: null, plan: '', profile_pic: ''});
+  
+  const [inputName, setInputName] = useState<string>(userInfo.name);
+  const [inputEmail, setInputEmail] = useState<string>(userInfo.email);
+  const [inputPhone, setInputPhone] = useState<string>(userInfo.phone ?? '');
+  const [inputPlan, setInputPlan] = useState<boolean>(userInfo.plan === 'vip' ? true : false);
+  
+  const [userDelPassword, setUserDelPassword] = useState<string>('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cookie = new Cookies();
+  
+  useEffect(() =>{
+    (async () =>{
+      let token: string = accessTState.accessToken;
 
-  const user = {
-    name: 'test',
-    email: 'test@test.test',
-    plan: 'vip',
-    phone: '+5584900000000'
+      if(!checkAccessToken(token)) token = '';
+
+      const refreshToken = cookie.get('RefreshToken');
+
+      if(!token && refreshToken){
+        token = await getAccessToken(refreshToken);
+        dispatch(setAccessToken(token));
+      } 
+
+      if(token){
+        const req = await fetch(import.meta.env.VITE_API_BASE_URL + 'user/' + id, {
+          headers: {
+            'method': 'get',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + token
+          }
+        });
+
+        const res = await req.json();
+        
+        if('user' in res){
+          let {name, email, phone, plan, profile_pic} = res.user;
+
+          setUserInfo({ name, email, phone, plan, profile_pic });
+          setInputName(name);
+          setInputEmail(email);
+          setInputPhone(phone);
+          setInputPlan(plan);
+        }else{
+          alert('Invalid User Id');
+          navigate('/');
+        }
+
+      }else navigate('/admin/login');
+    })();
+  }, []);
+
+  type Event = React.ChangeEvent<HTMLInputElement>;
+
+  const handleNameChange = (e: Event) =>{ setInputName(e.target.value) };
+  const handleEmailChange = (e: Event) =>{ setInputEmail(e.target.value) };
+  const handlePhoneChange = (e: Event) =>{ setInputPhone(e.target.value) };
+  const handlePlanChange = () =>{ setInputPlan(!inputPlan) };
+
+  const handleShowDelForm = () =>{ setShowDelForm(!showDelForm) };
+  const handleShowEditForm = () =>{ setShowEditForm(!showEditForm) };
+  const handleCam = () =>{ setShowCamIcon(!showCamIcon) };
+
+  const handleEdit = async () =>{
+    let newData: any = {};
+
+    if(inputName !== userInfo.name) newData.name = inputName;
+    if(inputEmail !== userInfo.email) newData.email = inputEmail;
+    if(inputPhone !== userInfo.phone) newData.phone = inputPhone;
+    if(inputPlan ? 'vip' : 'normal' !== userInfo.plan) newData.plan = inputPlan ? 'vip' : 'normal';
+
+    if(newData){
+      try{
+        const req = await fetch(import.meta.env.VITE_API_BASE_URL + 'user/' + id, {
+          method: 'put',
+          headers: {
+            'Authorization': 'Bearer ' + accessTState.accessToken,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newData)
+        });
+  
+        const res = await req.json();
+  
+        if('err' in res) alert(res.err);
+        else{
+          alert('User Updated.');
+
+          const {name, email, phone, plan, profile_pic} = res.user;
+          
+          setInputName(name);
+          setInputEmail(email);
+          setInputPhone(phone);
+          setInputPlan(plan === 'vip' ? true : false);
+          setUserInfo({name, email, phone, plan, profile_pic});
+        }
+      }catch(err){
+        alert('It happend an error while trying to update User.');
+      }
+    }else{
+      alert('User not Updated! You need to change and fill one or more fields.');
+    }
   }
 
-  const handleDeletion = () =>{
-    setShowDelForm(!showDelForm);
+  const handleChangeUserDelPassword = (e: Event) =>{ setUserDelPassword(e.target.value) }
+
+  const handleDel = async () =>{
+    if(userDelPassword){
+      let password = userDelPassword;
+      setUserDelPassword('');
+      
+      try{
+        const req = await fetch(import.meta.env.VITE_API_BASE_URL + 'user/' + id, {
+          method: 'delete',
+          headers: {
+            'Authorization': 'Bearer ' + accessTState.accessToken,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({password})
+        });
+  
+        const res = await req.json();
+
+        if('err' in res) alert('Error: ' + res.err);
+        
+        else{
+          alert('User Deleted');
+          navigate('/');
+        }
+      }catch(err){
+        alert('It happened an error while trying to delete User');
+      }
+    }
   }
 
-  const handleEdition = () =>{
-    setShowEditForm(!showEditForm);
-  }
-
-  const handleCam = () =>{
-    setShowCamIcon(!showCamIcon);
-  }
-
-  const handleClickProPic = (e: any) =>{
+  const handleProfilePicChange = (e: any) =>{
     const input: any = document.querySelector("#pic_file");
     if(input) input.click();
 
@@ -36,49 +171,55 @@ function User(){
 
   return (
     <Container>
-      <div className="userInfo">
-        <div onMouseEnter={handleCam} onMouseLeave={handleCam} onClick={handleClickProPic} className="profile_pic">
+      <div className="infoSpace">
+        <div onMouseEnter={handleCam} onMouseLeave={handleCam} onClick={handleProfilePicChange} className="profile_pic">
           <input id="pic_file" type="file" />
-          <img id="profile_pic" src="http://localhost:3000/public/assets/images/default.png" alt="User's profile picture" />
+          {userInfo.profile_pic &&
+            <img 
+              id="profile_pic" 
+              src={import.meta.env.VITE_BASE_URL + 'public/assets/images/' + userInfo.profile_pic} 
+              alt="User's profile picture"
+            />
+          }
           <div id="camera" style={{display: showCamIcon ? 'flex' : 'none'}}><img src="http://localhost:3000/public/assets/images/camera.png" alt="User's profile picture" /></div>
         </div>
 
+        {userInfo.email &&
+          <Info>
+            <li>Name: {userInfo.name}</li>
+            <li>Email: {userInfo.email}</li>
+            <li>Phone: {userInfo.phone ?? '(None)'}</li>
+            <li>Plan: {userInfo.plan}</li>
+            <li><button id="edit" onClick={handleShowEditForm}>Edit Information</button></li>
+            <li><button id="del" onClick={handleShowDelForm}>Delete User</button></li>
+          </Info>
+        }
 
-        <Info>
-          <li>Name: {user.name}</li>
-          <li>Email: {user.email}</li>
-          <li>Phone: {user.phone ?? '(None)'}</li>
-          <li>Plan: {user.plan}</li>
-          <li><button id="edit" onClick={handleEdition}>Edit Information</button></li>
-          <li><button id="del" onClick={handleDeletion}>Delete User</button></li>
-        </Info>
-
-        <div id="editForm" className="userForm" style={{display: showEditForm ? 'flex' : 'none'}}>
+        <div id="editForm" className="formContainer" style={{display: showEditForm ? 'flex' : 'none'}}>
           <div className="form">
-            <p className="X"><span onClick={handleEdition}>X</span></p>
+            <p className="X"><span onClick={handleShowEditForm}>X</span></p>
 
             <h4>Change User's information below</h4>
 
-            <input type="hidden" value={id} />
-            <input type="text" name="name" placeholder="Name" />
-            <input type="email" name="email" placeholder="Email" />
-            <input type="text" name="phone" placeholder="Phone" />
+            <input type="text" name="name" placeholder="Name" value={inputName} onChange={handleNameChange} />
+            <input type="email" name="email" placeholder="Email" value={inputEmail} onChange={handleEmailChange} />
+            <input type="text" name="phone" placeholder="Phone" value={inputPhone} onChange={handlePhoneChange} />
             <label>
-              <input type="checkbox" name="plan" placeholder="Plan" checked={user.plan === 'vip' ? true : false} /> VIP PLAN 
+              <input type="checkbox" name="plan" placeholder="Plan" checked={inputPlan} onChange={handlePlanChange} />&nbsp; VIP PLAN 
+              <div className="star"></div>
             </label>
 
-            <button>Edit</button>
+            <button onClick={handleEdit}>Edit</button>
           </div>
         </div>
 
-        <div id="delForm" className="userForm" style={{display: showDelForm ? 'flex' : 'none'}}>
+        <div id="delForm" className="formContainer" style={{display: showDelForm ? 'flex' : 'none'}}>
           <div className="form">
-            <p className="X"><span onClick={handleDeletion}>X</span></p>
-
+            <p className="X"><span onClick={handleShowDelForm}>X</span></p>
             <h4>Do you really want to delete this User?</h4>
+            <input type="password" name="password" placeholder="Admin's Password" value={userDelPassword} onChange={handleChangeUserDelPassword} />
 
-            <input type="hidden" value={id} />
-            <button>Delete</button>
+            <button onClick={handleDel}>Delete</button>
           </div>
         </div>
       </div>
