81d9964f9a8e5ee6b7e3ddce33476c6853d10b2d
Marcelo Augusto <maracelojob@gmail.com>
2024-04-09 11:30:33 -0300

    refreshRoute.test.ts added

---
+++ server/src/tests/integration/refreshRoute.test.ts
@@ -0,0 +1,35 @@
+import request from "supertest";
+import app from "../../app";
+import { MongoClear, MongoConnect, MongoDisconnect } from "../../database/mongo";
+
+describe('test refresh route', () =>{
+  let accessToken = '';
+  let refreshToken = '';
+
+  beforeAll(async () =>{
+    await MongoConnect();
+
+    await request(app)
+      .post('/api/admin/register')
+      .send('name=test&email=test@test.test&password=Test1test&password_confirmation=Test1test')
+    .then(res =>{
+      accessToken = `Bearer ${res.body.accessToken}`;
+      refreshToken = res.body.refreshToken;
+    });
+  });
+
+  afterAll(async () =>{
+    await MongoClear();
+    await MongoDisconnect();
+  });
+
+  it('need to return accessToken', async () =>{
+    await request(app)
+      .get('/api/refresh')
+      .set('Authorization', accessToken)
+      .set('Refresh-Token', refreshToken)
+    .then(res =>{
+      expect(res.body.accessToken).toBeDefined();
+    });
+  });
+});
