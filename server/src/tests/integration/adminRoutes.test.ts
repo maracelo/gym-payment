06ebe51b02032be68491b4ab08d672b06109a860
Marcelo Augusto <maracelojob@gmail.com>
2024-04-09 09:15:13 -0300

    Update image tests

--- server/src/tests/integration/adminRoutes.test.ts
+++ server/src/tests/integration/adminRoutes.test.ts
@@ -2,6 +2,8 @@ import request from "supertest";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import fs from "fs";
+import path from "path";
 
 import app from "../../app";
 import { MongoClear, MongoConnect, MongoDisconnect } from "../../database/mongo";
@@ -266,6 +268,114 @@ describe('test admin\'s routes', () =>{
                 .expect('Content-Type', /json/)
                 .expect(400)
             .then(res =>{ expect(res.body).toStrictEqual({err: 'Invalid phone number'}) })
+        });
+    });
+
+    it('should change profile pic', async () =>{
+        let profilePicName = '';
+
+        await request(app)
+            .put(`/api/admin/${firstAdminId}/newpic`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+            .set('Accept', 'application/json')
+            .attach('newPic', path.join(__dirname, 'testFiles/testImage.jpg'))
+        .then(res =>{
+            expect(res.body.admin.email).toBe('test3@test.test');
+            expect(res.body.admin.profile_pic).toBeDefined();
+            profilePicName = res.body.admin.profile_pic;
+        });
+
+        const file = fs.existsSync(path.join(__dirname, `../../../public/media/images/${profilePicName}`));
+        expect(file).toBe(true);
+        const tempFile = fs.existsSync(path.join(__dirname, `../../../public/media/images/temp/${profilePicName}`));
+        expect(tempFile).toBe(false);
+    });
+
+    it('should update and delete old profile pic', async () =>{
+        let oldProfilePicName = '';
+        let profilePicName = '';
+
+        await request(app)
+            .get(`/api/admin/${firstAdminId}`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+        .then(res =>{
+            oldProfilePicName = res.body.admin.profile_pic;
+        });
+
+        await request(app)
+            .put(`/api/admin/${firstAdminId}/newpic`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+            .set('Accept', 'application/json')
+            .attach('newPic', path.join(__dirname, 'testFiles/testImage.jpg'))
+        .then(res =>{
+            expect(res.body.admin.email).toBe('test3@test.test');
+            expect(res.body.admin.profile_pic).toBeDefined();
+            profilePicName = res.body.admin.profile_pic;
+        });
+        
+        const file = fs.existsSync(path.join(__dirname, `../../../public/media/images/${profilePicName}`));
+        expect(file).toBe(true);
+        const oldFile = fs.existsSync(path.join(__dirname, `../../../public/media/images/temp/${oldProfilePicName}`));
+        expect(oldFile).toBe(false);
+    });
+
+    it('should remove profile pic', async () =>{
+        let profilePicName = 'default_profile_pic.jpg'
+
+        await request(app)
+            .put(`/api/admin/${firstAdminId}/removepic`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+            .set('Accept', 'application/json')
+        .then(res =>{
+            expect(res.body.admin.profile_pic).toBe(profilePicName);
+        });
+
+        const file = fs.existsSync(path.join(__dirname, `../../../public/media/images/${profilePicName}`));
+        expect(file).toBe(true);
+        const tempFile = fs.existsSync(path.join(__dirname, `../../../public/media/images/temp/${profilePicName}`));
+        expect(tempFile).toBe(false);
+    });
+
+    describe('it should not change profile pic', ()=>{
+
+        it('should not accept a txt file', async () =>{
+            await request(app)
+                .put(`/api/admin/${firstAdminId}/newpic`)
+                .set('Authorization', accessToken)
+                .set('Refresh-Token', refreshToken)
+                .set('Accept', 'application/json')
+                .attach('newPic', path.join(__dirname, 'testFiles/text.txt'))
+            .then(res =>{
+                expect(res.body).toStrictEqual({err: 'Profile pic wasn\'t sent'});
+            });
+    
+            await request(app)
+                .get(`/api/admin/${firstAdminId}`)
+                .set('Authorization', accessToken)
+                .set('Refresh-Token', refreshToken)
+            .then(res =>{
+                const profilePic = res.body.admin.profile_pic;
+    
+                expect(profilePic).toBe('default_profile_pic.jpg');
+    
+                const tempFile = fs.existsSync(`../public/media/images/temp/${profilePic}`);
+                expect(tempFile).toBe(false);
+            });
+        });
+
+        it('should return profile pic not sent', async ()=>{
+            await request(app)
+                .put(`/api/admin/${firstAdminId}/newpic`)
+                .set('Authorization', accessToken)
+                .set('Refresh-Token', refreshToken)
+                .set('Accept', 'application/json')
+            .then(res =>{
+                expect(res.body).toStrictEqual({err: 'Profile pic wasn\'t sent'});
+            });
         });
     });
 
--- server/src/tests/integration/userRoutes.test.ts
+++ server/src/tests/integration/userRoutes.test.ts
@@ -1,4 +1,6 @@
 import request from "supertest";
+import fs from "fs";
+import path from "path";
 
 import app from "../../app";
 import { MongoClear, MongoConnect, MongoDisconnect } from "../../database/mongo";
@@ -7,6 +9,7 @@ import genRandPhoneNum from '../../helpers/genRandPhoneNum';
 
 describe("test user's routes", () =>{
     let firstUserId = '';
+    let secondUserId = '';
     let accessToken = '';
     let refreshToken = '';
 
@@ -122,6 +125,7 @@ describe("test user's routes", () =>{
             .set('Accept', 'application/json')
             .expect('Content-type', /json/)
             .expect(201)
+        .then(res =>{secondUserId = res.body.user._id})
     });
    
     it('should get 2 users', async () =>{
@@ -152,7 +156,7 @@ describe("test user's routes", () =>{
         });
 
         test('update email', async () =>{
-            const email = 'testt@test.test';
+            const email = 'test3@test.test';
 
             await request(app)
                 .put(`/api/user/${firstUserId}`)
@@ -255,6 +259,114 @@ describe("test user's routes", () =>{
                 .expect(400)
             .then(res =>{ expect(res.body).toStrictEqual({err: 'Invalid phone number'}) });
         })
+    });
+
+    it('should change profile pic', async () =>{
+        let profilePicName = '';
+
+        await request(app)
+            .put(`/api/user/${firstUserId}/newpic`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+            .set('Accept', 'application/json')
+            .attach('newPic', path.join(__dirname, 'testFiles/testImage.jpg'))
+        .then(res =>{
+            expect(res.body.user.email).toBe('test3@test.test');
+            expect(res.body.user.profile_pic).toBeDefined();
+            profilePicName = res.body.user.profile_pic;
+        });
+
+        const file = fs.existsSync(path.join(__dirname, `../../../public/media/images/${profilePicName}`));
+        expect(file).toBe(true);
+        const tempFile = fs.existsSync(path.join(__dirname, `../../../public/media/images/temp/${profilePicName}`));
+        expect(tempFile).toBe(false);
+    });
+
+    it('should update and delete old profile pic', async () =>{
+        let oldProfilePicName = '';
+        let profilePicName = '';
+
+        await request(app)
+            .get(`/api/user/${firstUserId}`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+        .then(res =>{
+            oldProfilePicName = res.body.user.profile_pic;
+        });
+
+        await request(app)
+            .put(`/api/user/${firstUserId}/newpic`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+            .set('Accept', 'application/json')
+            .attach('newPic', path.join(__dirname, 'testFiles/testImage.jpg'))
+        .then(res =>{
+            expect(res.body.user.email).toBe('test3@test.test');
+            expect(res.body.user.profile_pic).toBeDefined();
+            profilePicName = res.body.user.profile_pic;
+        });
+        
+        const file = fs.existsSync(path.join(__dirname, `../../../public/media/images/${profilePicName}`));
+        expect(file).toBe(true);
+        const oldFile = fs.existsSync(path.join(__dirname, `../../../public/media/images/temp/${oldProfilePicName}`));
+        expect(oldFile).toBe(false);
+    });
+
+    it('should remove profile pic', async () =>{
+        let profilePicName = 'default_profile_pic.jpg'
+
+        await request(app)
+            .put(`/api/user/${firstUserId}/removepic`)
+            .set('Authorization', accessToken)
+            .set('Refresh-Token', refreshToken)
+            .set('Accept', 'application/json')
+        .then(res =>{
+            expect(res.body.user.profile_pic).toBe(profilePicName);
+        });
+
+        const file = fs.existsSync(path.join(__dirname, `../../../public/media/images/${profilePicName}`));
+        expect(file).toBe(true);
+        const tempFile = fs.existsSync(path.join(__dirname, `../../../public/media/images/temp/${profilePicName}`));
+        expect(tempFile).toBe(false);
+    });
+
+    describe('it should not change profile pic', ()=>{
+
+        it('should not accept a txt file', async () =>{
+            await request(app)
+                .put(`/api/user/${firstUserId}/newpic`)
+                .set('Authorization', accessToken)
+                .set('Refresh-Token', refreshToken)
+                .set('Accept', 'application/json')
+                .attach('newPic', path.join(__dirname, 'testFiles/text.txt'))
+            .then(res =>{
+                expect(res.body).toStrictEqual({err: 'Profile pic wasn\'t sent'});
+            });
+    
+            await request(app)
+                .get(`/api/user/${firstUserId}`)
+                .set('Authorization', accessToken)
+                .set('Refresh-Token', refreshToken)
+            .then(res =>{
+                const profilePic = res.body.user.profile_pic;
+    
+                expect(profilePic).toBe('default_profile_pic.jpg');
+    
+                const tempFile = fs.existsSync(`../public/media/images/temp/${profilePic}`);
+                expect(tempFile).toBe(false);
+            });
+        });
+
+        it('should return profile pic not sent', async ()=>{
+            await request(app)
+                .put(`/api/user/${firstUserId}/newpic`)
+                .set('Authorization', accessToken)
+                .set('Refresh-Token', refreshToken)
+                .set('Accept', 'application/json')
+            .then(res =>{
+                expect(res.body).toStrictEqual({err: 'Profile pic wasn\'t sent'});
+            });
+        });
     });
 
     describe('it should delete user', () =>{
